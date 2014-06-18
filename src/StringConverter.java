d2ac2e4f17fbaef5fd983a18d6e60c4d1079cd60
jun85664396 <jun85664396@gmail.com>
2014-06-18 13:53:54 +0900

    -
    -

--- src/StringConverter.java
+++ src/StringConverter.java
@@ -1,13 +1,16 @@
 public class StringConverter {
-	public static String shortURL(long idx, String res) {
-		if (idx >= 52) {
-			long loop = (idx / 52) - 1;
-			idx -= (loop + 1) * 52;
-			res += shortURL(loop, res);
+	public static String res(long idx, String res) {
+		if (idx >= 62) {
+			long loop = (idx / 62) - 1;
+			idx -= (loop + 1) * 62;
+			res += res(loop, res);
 		}
-		if (idx >= 26) {
+		if (idx >= 36) {
 			idx += 6;
 		}
-		return res += (char) (idx + 65);
+		if(idx >= 10){
+			idx += 7;
+		}
+		return res += (char) (idx + 48);
 	}
 }
