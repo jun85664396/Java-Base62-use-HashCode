e47aa499eca1c92da10ac8d2bf1f74ab5d9f082a
JunSangPil <jun85664396@gmail.com>
2014-06-19 16:36:11 +0900

    add decoding

--- test/StringConverterTest.java
+++ test/StringConverterTest.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 
-public class StringConverterTest {
-
-	public static void main(String[] args) {
+public class StringConverterTest{
+	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		long idx;
 		String res;
@@ -12,21 +11,49 @@ public class StringConverterTest {
 			if (idx == -1) {
 				break;
 			}
-			System.out.println(res(idx, res));
+			res = encoding(idx, res);
+			System.out.println("encoding : " + res);
+			System.out.println("decoding : " + decoding(res));
 		}
 	}
-	public static String res(long idx, String res) {
+
+	public static String encoding(long idx, String res) {
 		if (idx >= 62) {
 			long loop = (idx / 62) - 1;
 			idx -= (loop + 1) * 62;
-			res += res(loop, res);
+			res += encoding(loop, res);
 		}
 		if (idx >= 36) {
 			idx += 6;
 		}
-		if(idx >= 10){
+		if (idx >= 10) {
 			idx += 7;
 		}
 		return res += (char) (idx + 48);
+	}
+
+	public static long decode(String res) {
+		String capitalLetter = "^[A-Z]";
+		String smallLetter = "^[a-z]";
+		if (res.matches(capitalLetter)) {
+			return res.hashCode() - 48 - 7;
+		} else if (res.matches(smallLetter)) {
+			return res.hashCode() - 48 - 6 - 7;
+		} else {
+			return res.hashCode() - 48;
+		}
+	}
+
+	public static long decoding(String res) {
+		long output = 0;
+		for (int i = res.length(); i > 0; i--) {
+			if (i == res.length())
+				output += decode(res.substring(i - 1, i));
+			else {
+				output += (Math.pow(62, res.length() - i))
+						* (decode(res.substring(i - 1, i)) + 1);
+			}
+		}
+		return output;
 	}
 }
